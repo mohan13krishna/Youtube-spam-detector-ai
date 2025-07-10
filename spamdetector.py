31cb8316b08a7e45fa8f1b149de208377b741102
Mohan Krishna Thalla <142295454+mohan13krishna@users.noreply.github.com>
2025-07-10 18:24:38 +0530

    Update spamdetector.py

--- spamdetector.py
+++ spamdetector.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
-import torch
+import torch 
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer, BertForSequenceClassification  
 from torch.optim import AdamW 
